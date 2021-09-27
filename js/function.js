0ecad078a4ce9387e4d6ddc851af1351e8270698
Vinicius Ferreira <viniciuferreira32@gmail.com>
2021-09-26 22:30:46 -0300

    falta muita coisa ainda

--- js/function.js
+++ js/function.js
@@ -1,3 +1,38 @@
 $(function(){
+    //Variavies
+    var number = $('.number');
+    var signal = $('.signal');
+    var tela = $('.valor');
+    var result = $('#igual');
     
+    //Funções
+   // console.log(amt)
+    
+    number.click(function(){
+        tela.append($(this).val());
+
+        if($(this).index() == 7){
+            $('.n').click(function(){
+                var n = $(this).val();
+                var re = n + n;
+                result.click(function(){
+                    tela.html(re);  
+                      
+            })
+            
+            })
+        }
+    
+    })    
+
+       /* signal.click(function(){
+            result.prepend('<p class="valor">'+ $(this).val()+'</p>');
+            return result;
+        });
+        */
+
+
 })
+
+
+
